1a49d3f9e8ab06be4c891914593286679c415cd2
luyang00 <393780933@qq.com>
2015-03-25 20:17:44 +0800

    Add F4Timer.cpp F4Timer.h Signed-off-by: luyang00 <393780933@qq.com>

--- HAL/Interface/Timer.h
+++ HAL/Interface/Timer.h
@@ -7,7 +7,7 @@ namespace HAL
 	class Timer
 	{
 	public:
-		virtual void set_period() = 0;				// micro-second
+		virtual void set_period(uint32_t period) = 0;				// micro-second
 		virtual void set_callback(int ms) = 0;		
 	};
 }
