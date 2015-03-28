d08628aea40cf9f204d74e546c0a1fdbec702aab
my12doom <my12doom@126.com>
2015-03-28 14:47:33 +0800

    changed register accessing functions of MPU6000/HMC5983 from protected to public.

--- BSP/devices/sensors/HMC5983SPI.h
+++ BSP/devices/sensors/HMC5983SPI.h
@@ -16,11 +16,11 @@ namespace sensors
 		// data[0 ~ 7] :
 		// accel_x, accel_y, accel_z, raw_temperature, gyro_x, gyro_y, gyro_z
 		int read(short *data);
-
-	protected:
 		int read_reg(uint8_t reg, void *out, int count);
 		int write_reg_core(uint8_t reg, uint8_t data);
 		int write_reg(uint8_t reg, uint8_t data);
+
+	protected:
 		HAL::ISPI *spi;
 		HAL::IGPIO *CS;
 	};
