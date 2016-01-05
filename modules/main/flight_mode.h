6c0d0896de4e4286c67bbf6c22a0b504a0159ed3
my12doom <my12doom@126.com>
2016-01-05 16:04:24 +0800

    flight mode interface description.

--- modules/main/flight_mode.h
+++ modules/main/flight_mode.h
@@ -15,3 +15,16 @@ public:
 	// main loop, called in main loop
 	virtual int loop(float dt) = 0;
 };
+
+
+// to implement a new flight mode:
+
+// setup each controllers/variables needed in setup(), return 0 on successs, negativa value for failuere.
+
+// run all position or other controllers and output attitude and throttle setpoint by calling
+// yap.attitude_controller.set_euler_target() or yap.attitude_controller.update_target_from_stick();
+// and set yap.throttle_result to wanted value.
+//   you can use yap.alt_controller.update(rate); then yap.throttle_result = yap.alt_controller.get_result()
+//   or call yap.default_alt_controlling() to use default alt holding behavior.
+
+// do cleanup in exit();
