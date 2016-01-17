be01c4ae33d3662631ae8ccb5c171ec753c1e742
my12doom <my12doom@126.com>
2016-01-17 16:04:39 +0800

    updated RC interface.

--- HAL/Interface/IRCIN.h
+++ HAL/Interface/IRCIN.h
@@ -4,6 +4,11 @@
 
 namespace HAL
 {
+	enum RCIN_State
+	{
+		RCIN_Normal,
+		RCIN_Fail,
+	};
 	// channel index starts from 0
 	class IRCIN
 	{
@@ -20,5 +25,6 @@ namespace HAL
 		// statistics functions is mainly for RC calibration purpose.
 		virtual int get_statistics_data(int16_t *min_out, int16_t *max_out, int start_channel, int max_count) = 0;
 		virtual int reset_statistics() = 0;
+		virtual RCIN_State state(){return RCIN_Normal;}		// default state handling since most RC don't have a explicit failure definition.
 	};
 }
