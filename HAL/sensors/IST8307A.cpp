fd5619f3733d98c7e599f0c4feb75aeb5ac2efb2
my12doom <my12doom@126.com>
2016-02-16 16:51:43 +0800

    IST8307A sensitivity update.

--- HAL/sensors/IST8307A.cpp
+++ HAL/sensors/IST8307A.cpp
@@ -203,9 +203,9 @@ int IST8307A::read(devices::mag_data *out)
 	}
 	
 	
-	out->x = last_data[axis[0]] * 0.03f * negtive[0];		// to milli-gauss (1mGauss = 0.1 uT, sensitivity = 3.3 LSB/uT = 33LSB/mGauss)
-	out->y = last_data[axis[1]] * 0.03f * negtive[1];
-	out->z = last_data[axis[2]] * 0.03f * negtive[2];
+	out->x = last_data[axis[0]] * 3.00f * negtive[0];		// to milli-gauss (1mGauss = 0.1 uT, sensitivity = 3.3 LSB/uT = 0.33LSB/mGauss)
+	out->y = last_data[axis[1]] * 3.00f * negtive[1];
+	out->z = last_data[axis[2]] * 3.00f * negtive[2];
 	out->temperature = NAN;
 	
 	return 0;
