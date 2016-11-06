943201eae2a4e60ba7b03cdd0dbc7fb49e12264d
my12doom <my12doom@126.com>
2016-11-06 18:48:04 +0800

    ICamera update.

--- HAL/Interface/ICamera.h
+++ HAL/Interface/ICamera.h
@@ -6,9 +6,11 @@ namespace devices
 {
 	enum pixel_type
 	{
-		L8,
-		YUY2,
-		RGB565,
+		L8 = 0,
+		YUY2 = 1,
+		RGB565 = 2,
+		YV12 = 3,
+		NV12 = 4,
 	};
 
 	typedef struct _frame_format
@@ -19,23 +21,42 @@ namespace devices
 		int pixel_type;
 	} frame_format;
 
+	typedef struct _time_stamp
+	{
+		int64_t start;		// start in HAL::ISysTimer units
+		int64_t length;		// length in HAL::ISysTimer units
+	} timestamp;
+
 	class ICameraCallback
 	{
 	public:
-		virtual void on_frame() = 0;
-		virtual void on_line() = 0;
+		virtual void on_frame(timestamp ts) = 0;
+		virtual void on_line(timestamp ts) = 0;
 		virtual void on_event(int eventcode) = 0;
 	};
 
 	class ICamera
 	{
 	public:
-		virtual int get_frame(void **pp, int64_t *timestamp=NULL) = 0;
+		// get one frame from the camere's internal queue.
+		// pp: out pointer
+		// timestamp: out pointer
+		// only_latest: if true, discard all frame except the latest one.
+		// return: 0 if new frame retrived, 1 if no new data, negative values for error.
+		virtual int get_frame(uint8_t **pp, timestamp *timestamp=NULL, bool only_latest = false) = 0;
 
+		// get current frame format
 		virtual int get_frame_format(frame_format *format) = 0;
 
+		// get available frame format.
+		virtual int get_available_frame_format(int index, frame_format *format) = 0;
+		int get_available_frame_format_count();
+
+		// set frame format.
+		// return 0 if successed, -1 if failed.
 		virtual int set_frame_format(const frame_format *format) = 0;
-		//
+
+		// set a callback
 		virtual int set_callback(ICameraCallback *cb) = 0;
 
 		// return false if any error/waning
