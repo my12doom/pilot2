9f28ad7f3a9995c5b00ae5db162c17a931a21b76
my12doom <my12doom@126.com>
2017-06-01 18:54:19 +0800

    added: F0 UART. note: glitch tx dma.

---
+++ HAL/STM32F0/F0UART.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <HAL/Interface/IUART.h>
+#include <stm32F0xx_usart.h>
+#include <stm32F0xx_dma.h>
+
+#define TX_BUFFER_SIZE 200
+#define RX_BUFFER_SIZE 256
+
+namespace STM32F0
+{
+	class F0UART:public HAL::IUART
+	{
+	public:
+		F0UART(USART_TypeDef * USARTx);
+		~F0UART(){};
+
+		virtual int set_baudrate(int baudrate);
+		virtual int write(const void *data, int count);
+		virtual int flush();
+		virtual int read(void *data, int max_count);
+		virtual int peak(void *data, int max_count);
+		virtual int readline(void *data, int max_count);
+		virtual int available();
+
+		void usart_irq(void);
+		void dma_irq();
+			
+		void destroy();
+
+	private:
+		USART_TypeDef * USARTx;
+		int baudrate;
+
+		// TX DMA var:
+		DMA_Channel_TypeDef* tx_DMAy_Streamx;
+		volatile bool tx_dma_running;
+		int tx_start;
+		int tx_end;
+		int ongoing_tx_size;
+
+		// RX buffer:
+		char rx_buffer[RX_BUFFER_SIZE];		// circular buffer
+		int start;
+		int end;
+		int ongoing_rx_start;
+		int ongoing_rx_size;
+		volatile bool rx_dma_running;
+
+		char tx_buffer[TX_BUFFER_SIZE];
+
+		void dma_tx_init();
+		int dma_handle_tx_queue();
+	};
+}
