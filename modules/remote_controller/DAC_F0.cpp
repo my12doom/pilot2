90abc2c2b77b72cc57500f536b867f19a31e90ba
my12doom <my12doom@126.com>
2017-06-24 18:01:26 +0800

    xb TX: DAC works.

--- modules/remote_controller/DAC_F0.cpp
+++ modules/remote_controller/DAC_F0.cpp
@@ -10,14 +10,16 @@
 using namespace STM32F0;
 using namespace HAL;
 
-static uint8_t data[2] = {0xff};
-F0GPIO *beep_en = NULL;
-bool polary = false;
+static F0GPIO *beep_en = NULL;
+bool reverse_polary = false;
+static uint32_t repeat = 1;
 
-int dac_config(STM32F0::F0GPIO *en, bool _polary)
+int dac_config(STM32F0::F0GPIO *en, bool _reverse_polary)
 {
 	beep_en = en;
-	polary = _polary;
+	reverse_polary = _reverse_polary;
+	beep_en->write(false ^ reverse_polary);
+	beep_en->set_mode(MODE_OUT_PushPull);
 
  	RCC_APB1PeriphClockCmd(RCC_APB1Periph_DAC, ENABLE);
   	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
@@ -47,9 +49,9 @@ int dac_config(STM32F0::F0GPIO *en, bool _polary)
 	DMA_InitTypeDef DMA_InitStructure;
 	DMA_DeInit(DMA1_Channel3);
 	DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&DAC->DHR8R1;
-	DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)&data;
+	DMA_InitStructure.DMA_MemoryBaseAddr = 0;
 	DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
-	DMA_InitStructure.DMA_BufferSize = sizeof(data);
+	DMA_InitStructure.DMA_BufferSize = 0;
 	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
 	DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
 	DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
@@ -58,7 +60,6 @@ int dac_config(STM32F0::F0GPIO *en, bool _polary)
 	DMA_InitStructure.DMA_Priority = DMA_Priority_High;
 	DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
 	DMA_Init(DMA1_Channel3, &DMA_InitStructure);
-	DMA_Cmd(DMA1_Channel3, ENABLE);
 
 	DAC_InitTypeDef DAC_InitStruct;
 	DAC_InitStruct.DAC_Trigger = DAC_Trigger_T15_TRGO;
@@ -67,17 +68,38 @@ int dac_config(STM32F0::F0GPIO *en, bool _polary)
 	
 	DAC_Cmd(DAC_Channel_1, ENABLE);
 	DAC_DMACmd(DAC_Channel_1, ENABLE);
-		
+	
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_InitStructure.NVIC_IRQChannel = DMA1_Channel2_3_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPriority = 3;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+	DMA_ITConfig(DMA1_Channel3, DMA_IT_TC, ENABLE);
+	
+	
 	return 0;
+}
+
+extern "C" void DMA1_Channel2_3_IRQHandler()
+{
+	DMA_ClearFlag(DMA1_FLAG_TC3);
+	if (repeat == 0)
+	{
+		beep_en->write(true ^ reverse_polary);
+		DMA_Cmd(DMA1_Channel3, DISABLE);
+	}
+	repeat --;
 }
 
 int dac_run(const void *data, int data_point_count, int data_type, int repeat/* = 1*/)
 {	
-	int i=0;
-	uint8_t *data8 = (uint8_t*)data;
-	while(0)
-	{
-	}
+	beep_en->write(false ^ reverse_polary);
+	DMA_Cmd(DMA1_Channel3, DISABLE);
+	DMA1_Channel3->CNDTR = data_point_count;
+	DMA1_Channel3->CMAR = (uint32_t)data;
+	::repeat = repeat;
+	beep_en->write(false ^ reverse_polary);
+	DMA_Cmd(DMA1_Channel3, ENABLE);
 	
 	return 0;
 }
